7298ade97181d8ca0c270732426f4d33a8d1b22f
Abi L <124858748+lailiel@users.noreply.github.com>
2023-06-08 22:03:36 -0500

    fixed shapes.js file. index will now correctly return shape. text still needed

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
-const inquirer = require("inquirer");
-const fs = require("fs");
-const logoSVG = require("./utils/generateLogo");
+// const CLI = require('./lib/cli.js');
 
-const promptUser = () => {
-    return inquirer.prompt([
-    {
-      type: 'input',
-      message: 'Enter your 3 character logo text',
-      name: 'text',
-      maxLength:3
-    },
-    {
-       type: 'input',
-       message: 'Enter text color',
-       name: 'textColor' 
-    },
-    {
-        type: 'list',
+// const cli = new CLI();
+
+// cli.run();
+
+const inquirer = require("inquirer");
+const { Circle, Triangle, Square } = require("./lib/shapes");
+
+function main() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
         message: "Select a logo shape",
-        choices: [
-            'circle',
-            'triange',
-            'square'
-        ],
-        name: 'shape'
-    },
-    {
-        type: 'input',
+        choices: ["circle", "triangle", "square"],
+        name: "shape",
+      },
+      {
+        type: "input",
         message: "Enter logo shape color",
-        name: 'shapeColor'
-    }
-  ])
+        name: "shapeColor",
+      },
+    ])
+    .then((answers) => {
+      let shape;
+
+      if (answers.shape == "circle") {
+        shape = new Circle(answers.shapeColor);
+      }
+
+      if (answers.shape == "triangle") {
+        shape = new Triangle(answers.shapeColor);
+      }
+
+      if (answers.shape == "square") {
+        shape = new Square(answers.shapeColor);
+      }
+
+      console.log(shape);
+      console.log(shape.render());
+
+      
+    });
 }
 
-
-
-
-const init = () => {
-    promptUser()
-      .then((answers) => fs.writeFile('logo.svg', logoSVG({...answers}), (err)=> console.log(err))) 
-      .then(() => console.log('Generated logo.svg'))
-      .catch((err) => console.error(err));
-  };
-
-
-init();
+main();
