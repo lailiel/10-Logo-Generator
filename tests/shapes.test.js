ca1d4d5d6e80551344daeeade54e52da5a6c968e
Abi L <124858748+lailiel@users.noreply.github.com>
2023-06-19 20:07:37 -0500

    all tests for shapes pass, attempts to simplified code were unsuccessful, but it works as is

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -11,3 +11,36 @@ describe('Shapes', () => {
     })
 })
 
+describe('Shapes', () => {
+    describe('circle', () => {
+        it('Should produce a red circle', () => {
+            const shape = new Circle();
+            shape.setColor("red");
+            expect(shape.render()).toEqual(`<circle cx="150" cy="100" r="100" fill="red" />`);
+        })
+            
+    })
+})
+
+describe('Shapes', () => {
+    describe('square', () => {
+        it('Should produce a green square', () => {
+            const shape = new Square();
+            shape.setColor("green");
+            expect(shape.render()).toEqual(`<polygon points="50 0, 250 0, 250 200, 50 200" fill="green" />`);
+        })
+            
+    })
+})
+
+describe('Shapes', () => {
+    describe('diamond', () => {
+        it('Should produce a purple diamond', () => {
+            const shape = new Diamond();
+            shape.setColor("purple");
+            expect(shape.render()).toEqual(`<polygon points="50 100, 150 0, 250 100, 150 200" fill="purple" />`);
+        })
+            
+    })
+})
+
